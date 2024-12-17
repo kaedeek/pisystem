12841466d02c140b4ea305b906c8c5d0c512d591
kaedeek <yukise30@icloud.com>
2024-12-18 00:42:09 +0900

    Change the output from plain to colored.

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+from colorama import init, Fore, Style
+
 import subprocess
 import json
+import logging
+
+init(autoreset=True)
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+logger = logging.getLogger(__name__)
 
 def update_package():
     result = subprocess.run(["pip", "list", "--outdated", "--format=json"], capture_output=True, text=True)
     packages = json.loads(result.stdout)
 
     if not packages:
-        print("[Notice]  No outdated packages found :(")
+        logging.warning(Style.BRIGHT + Fore.YELLOW  + "No outdated packages found :(")
         return
     
     def upgrade_package(pip_name):
-        print(f"[Notice]  Updating {pip_name}...")
+        logger.info(Fore.GREEN + f"Updating {pip_name}...")
         subprocess.run(["pip", "install", "--upgrade", pip_name], check=True)
 
     for pip in packages:
         upgrade_package(pip["name"])
-    print("[Notice]  All packages updated successfully :)")
+    logger.info(Fore.GREEN + "All packages updated successfully :)")
 
 if __name__ == "__main__":
     update_package()
